ff8b8384d8a8b8c160d25ae77f20eab1c7c9c7d0
Austro <jgomez@software-global.com>
2018-05-26 05:57:34 -0600

    Musica y enseñansa directa

--- src/ChatVisual.java
+++ src/ChatVisual.java
@@ -2,10 +2,10 @@
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,6 +19,10 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import org.json.JSONArray;
 import org.json.JSONException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 
 public class ChatVisual extends javax.swing.JFrame {
 
@@ -29,79 +33,142 @@ public class ChatVisual extends javax.swing.JFrame {
     private String bienvenida = "YUAWI: Hola!";
     boolean aprendiendo = false;
     private String questionFail;
-    private String nameGamer;
+    private String nameGamer = "Tu";
     private Integer questionTurno = 0;
     private ArrayList<HashMap<String, String>> arrData;
 
-    private boolean simulateWrite=false;
+    private boolean simulateWrite = false;
+    private boolean ensenando = false;
+    Player apl = null;
+    private boolean cantar = true;
 
     //MAIN METHOD
     public ChatVisual() {
-        
+
         String nombreJugador = JOptionPane.showInputDialog(
-                                null,"Introduzca su nombre",
-                                "Bienvenido, antes de comenzar",
-                                JOptionPane.QUESTION_MESSAGE);
-        nameGamer = nombreJugador;
+                null, "Introduzca su nombre",
+                "Bienvenido, antes de comenzar",
+                JOptionPane.QUESTION_MESSAGE);
+        if (!nombreJugador.equals("")) {
+            nameGamer = nombreJugador;
+        }
         initComponents();
         setSize(405, 590);
         setLocationRelativeTo(null);
-        entrada.requestFocus();        
-        
-        this.getContentPane().setBackground(new java.awt.Color(234, 237, 237 ));                
-        
+        entrada.requestFocus();
+
+        this.getContentPane().setBackground(new java.awt.Color(234, 237, 237));
+
         contenido.setLineWrap(true);//Oculta scroll horizontal
-        contenido.setFont(new Font("Serif",Font.PLAIN,20));
-        contenido.setBackground(new java.awt.Color(31, 97, 141));
+        contenido.setFont(new Font("Serif", Font.PLAIN, 20));
+        contenido.setBackground(new java.awt.Color(46, 64, 83));
         contenido.setEditable(false);
         contenido.setForeground(new java.awt.Color(255, 255, 255));
-        
-        btnSend.setBackground(new java.awt.Color(84, 153, 199));
-        
-        addTextToInput(bienvenida);
+
+        btnTeacher.setBackground(new java.awt.Color(125, 60, 152));
+        btnSend.setBackground(new java.awt.Color(36, 113, 163));
+        btnSong.setBackground(new java.awt.Color(36, 113, 163));
+
+        addTextToInputOnly(bienvenida);
 
         arrData = new ArrayList<HashMap<String, String>>();
 
         addCerebro();
     }
-        
+
+    public void cantar(boolean canta) {
+        if (apl == null) {
+
+            try {
+                try {
+                    apl = new Player(new FileInputStream(
+                            "C:\\Users\\Austro\\Documents\\Develoment\\Java\\Yuawi Chatbot\\src\\audio.mp3"));
+                } catch (JavaLayerException ex) {
+                    Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+//        try {
+//            if(canta){                
+//                apl.play();
+//            }else{
+//                apl.close();
+//            }
+//        } catch (JavaLayerException ex) {
+//            Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+        new Thread() {
+            public void run() {
+                try {
+                    while (true) {
+                        if (!cantar) {
+                            if (!apl.play(1)) {
+                                break;
+                            }
+                        }
+                    }
+                } catch (JavaLayerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        cantar = true;
+    }
+
     //TAREAS
     public void addCerebro() {
-        String jsonData = readFile("cerebro.txt");          
+        String jsonData = readFile("cerebro.txt");
         JSONArray json = null;
         try {
             json = new JSONArray(jsonData);
         } catch (JSONException ex) {
             Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         for (int i = 0; i < json.length(); i++) {
             try {
                 HashMap<String, String> row = new HashMap<String, String>();
                 row.put("question", json.getJSONObject(i).get("question").toString());
                 row.put("response", json.getJSONObject(i).get("response").toString());
                 row.put("intent", json.getJSONObject(i).get("intent").toString());
-                arrData.add(row);                
+                arrData.add(row);
             } catch (JSONException ex) {
                 Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         System.out.println("Finishloading");
     }
+
     public void addText(String str) {
         contenido.setText(entrada.getText() + str);
-    }    
-    void addTextToInput(String suma) {
+    }
+
+    void addTextToInputOnly(String suma) {
         contenido.setText(contenido.getText() + suma);
         entrada.setText("");
     }
-    public void simulateWriting(){
-        
-        int max=6;
-        int min=3;
+
+    void addTextToInput(String suma) {
+        contenido.setText(contenido.getText() + "\n " + "\n " + suma);
+        entrada.setText("");
+    }
+
+    public void simulateWriting() {
+
+        int max = 6;
+        int min = 3;
         Random r = new Random();
         int res = r.nextInt((max - min) + 1) + min;
-        
+
         try {
             //Simula esperar un momento
             TimeUnit.SECONDS.sleep(res);
@@ -109,21 +176,32 @@ public class ChatVisual extends javax.swing.JFrame {
             Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     public void writeInChat() {
         String txt = entrada.getText();
         entrada.setText("");
-        
+
         //Simula escribir
-        if(simulateWrite)
-            simulateWriting();                
-        
-        addTextToInput("\n "+"\n "+nameGamer+":" + cleanOnlyTilde(txt)); //Escribe lo que acabas de nombrar
+        if (simulateWrite) {
+            simulateWriting();
+        }
+
+        addTextToInput(nameGamer + ":" + cleanOnlyTilde(txt)); //Escribe lo que acabas de nombrar
         String txtClean = clean(txt);
 
         //Si existe instruccion de borrado
         int intIndexDel = txtClean.indexOf(kamikaze);
         if (intIndexDel != -1) {
             deleteMemory(txtClean);
+            return;
+        }
+
+        //Se enseña al robot
+        if (ensenando) {
+            comoRespondoEso();
+            questionFail = txt;
+            ensenando = false;
+            aprendiendo = true;
             return;
         }
 
@@ -166,7 +244,7 @@ public class ChatVisual extends javax.swing.JFrame {
                             Integer intetRow = Integer.parseInt(value);
                             //Escoger la de menor tamaño
                             if ((intetRow <= lastIntent) || firstResponse) {
-                                if(!respuesta.equals("") && !respuesta.equals(" ")){                                    
+                                if (!respuesta.equals("") && !respuesta.equals(" ")) {
                                     respuestaEnd = respuesta;
                                 }
                                 iWin = i;
@@ -190,9 +268,10 @@ public class ChatVisual extends javax.swing.JFrame {
             moreOneArrData(iWin);
             //Pintar respuesta
             String resBot = cleanOnlyTilde(respuestaEnd);
-            addTextToInput("\n " +"\n "+ nameRobot + ":" +resBot.replace("�", ""));
-        }        
-    }    
+            addTextToInput(nameRobot + ":" + resBot.replace("�", ""));
+        }
+    }
+
     public void deleteMemory(String txt) {
 
         String cleanKeyDel = clean(txt.replace(kamikaze, ""));
@@ -221,6 +300,7 @@ public class ChatVisual extends javax.swing.JFrame {
         //Guardar en json txt
         saveJsonToTxt();
     }
+
     public void responseRemoveMemory(boolean eliminado) {
         String respuesta = "";
         switch (questionTurno) {
@@ -230,7 +310,7 @@ public class ChatVisual extends javax.swing.JFrame {
                 } else {
                     respuesta = nameRobot + ": No puedo olvidar eso, ni siquiera lo recuerdo";
                 }
-                questionTurno = +1;
+                questionTurno = questionTurno + 1;
                 break;
             case 1:
                 if (eliminado) {
@@ -238,7 +318,7 @@ public class ChatVisual extends javax.swing.JFrame {
                 } else {
                     respuesta = nameRobot + ": No lo he podido olvidar, talvez si lo intentas diferente";
                 }
-                questionTurno = +1;
+                questionTurno = questionTurno + 1;
                 break;
             case 2:
                 if (eliminado) {
@@ -251,6 +331,7 @@ public class ChatVisual extends javax.swing.JFrame {
         }
         addTextToInput("\n" + respuesta);
     }
+
     public void saveJsonToTxt() {
         //Lo guarda en un json
         JSONArray json = new JSONArray(arrData);
@@ -266,17 +347,18 @@ public class ChatVisual extends javax.swing.JFrame {
             Logger.getLogger(ChatVisual.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     public void responderQueAprendio() {
 //        int randomNumber = random.nextInt(max + 1 - min) + min;
         String respuesta = "";
         switch (questionTurno) {
             case 0:
                 respuesta = nameRobot + ": Entendido, asi contestare";
-                questionTurno = +1;
+                questionTurno = questionTurno + 1;
                 break;
             case 1:
                 respuesta = nameRobot + ": Ok! me parece perfecto, esa sera mi respuesta";
-                questionTurno = +1;
+                questionTurno = questionTurno + 1;
                 break;
             case 2:
                 respuesta = nameRobot + ": Me parece! tu si que eres bueno comunicandote";
@@ -285,6 +367,27 @@ public class ChatVisual extends javax.swing.JFrame {
         }
         addTextToInput("\n" + respuesta);
     }
+
+    public void comoRespondoEso() {
+//        int randomNumber = random.nextInt(max + 1 - min) + min;
+        String respuesta = "";
+        switch (questionTurno) {
+            case 0:
+                respuesta = nameRobot + ": y que respondo a eso?";
+                questionTurno = questionTurno + 1;
+                break;
+            case 1:
+                respuesta = nameRobot + ": como puedo contestar eso?";
+                questionTurno = questionTurno + 1;
+                break;
+            case 2:
+                respuesta = nameRobot + ": dime la mejor manera de conetestar eso! :)";
+                questionTurno = 0;
+                break;
+        }
+        addTextToInput("\n" + respuesta);
+    }
+
     public void aprender(String responseWin) {
         HashMap<String, String> row = new HashMap<String, String>();
         row.put("question", questionFail);
@@ -300,16 +403,17 @@ public class ChatVisual extends javax.swing.JFrame {
         //GUARDAR JSON EN TXT
         saveJsonToTxt();
     }
+
     public void preguntarComoAprender() {
         String pregunta = "";
         switch (questionTurno) {
             case 0:
-                pregunta = nameRobot + ": Disculpame pero no soy tan inteligente como un humano\n ¿Que puedo responder cuando me digan eso?";
-                questionTurno = +1;
+                pregunta = nameRobot + ": Disculpame pero me faltan cosas por aprender\n ¿Que puedo responder cuando me digan eso?";
+                questionTurno = questionTurno + 1;
                 break;
             case 1:
                 pregunta = nameRobot + ": Huy! eso no lo se responder ¿Tu me dirias como hacerlo?";
-                questionTurno = +1;
+                questionTurno = questionTurno + 1;
                 break;
             case 2:
                 pregunta = nameRobot + ": Upps! perdon ¿Me dices como responder a eso?";
@@ -318,6 +422,26 @@ public class ChatVisual extends javax.swing.JFrame {
         }
         addTextToInput("\n" + pregunta);
     }
+
+    public void preguntarQueAprender() {
+        String pregunta = "";
+        switch (questionTurno) {
+            case 0:
+                pregunta = nameRobot + ": Listo! que aprendo";
+                questionTurno = questionTurno + 1;
+                break;
+            case 1:
+                pregunta = nameRobot + ": Ok! dime la frase";
+                questionTurno = questionTurno + 1;
+                break;
+            case 2:
+                pregunta = nameRobot + ": De acuerdo! escribemelo";
+                questionTurno = 0;
+                break;
+        }
+        addTextToInput("\n" + pregunta);
+    }
+
     public void moreOneArrData(Integer iWin) {
         HashMap<String, String> row = new HashMap<String, String>();
         String question = arrData.get(iWin).get("question").toString();
@@ -329,6 +453,7 @@ public class ChatVisual extends javax.swing.JFrame {
         row.put("intent", intentSum.toString());
         arrData.set(iWin, row);
     }
+
     public void keyReleased(KeyEvent evt) {
         if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
             entrada.setEditable(true);
@@ -351,7 +476,8 @@ public class ChatVisual extends javax.swing.JFrame {
         output = output.replace(".", "");
         output = output.replace("  ", " ");
         return output.trim().toLowerCase();
-    }    
+    }
+
     public static String cleanOnlyTilde(String str) {
         String original = "áàäéèëíìïóòöúùuñÁÀÄÉÈËÍÌÏÓÒÖÚÙÜÑçÇ";
         String ascii = "aaaeeeiiiooouuunAAAEEEIIIOOOUUUNcC";
@@ -361,8 +487,9 @@ public class ChatVisual extends javax.swing.JFrame {
         }
         return output.trim();
     }
-    public String readFile(String path){
-        String everything="";
+
+    public String readFile(String path) {
+        String everything = "";
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(path));
@@ -398,7 +525,6 @@ public class ChatVisual extends javax.swing.JFrame {
         return everything;
     }
 
-    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -415,6 +541,8 @@ public class ChatVisual extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         btnSend = new javax.swing.JButton();
+        btnTeacher = new javax.swing.JButton();
+        btnSong = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -472,6 +600,28 @@ public class ChatVisual extends javax.swing.JFrame {
         getContentPane().add(btnSend);
         btnSend.setBounds(280, 510, 100, 40);
 
+        btnTeacher.setFont(new java.awt.Font("DokChampa", 0, 18)); // NOI18N
+        btnTeacher.setForeground(new java.awt.Color(255, 255, 255));
+        btnTeacher.setText("Enseñar");
+        btnTeacher.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTeacherActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnTeacher);
+        btnTeacher.setBounds(10, 510, 110, 40);
+
+        btnSong.setFont(new java.awt.Font("DokChampa", 0, 10)); // NOI18N
+        btnSong.setForeground(new java.awt.Color(255, 255, 255));
+        btnSong.setText("Canta we :v");
+        btnSong.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSongActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnSong);
+        btnSong.setBounds(300, 100, 90, 30);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
@@ -493,6 +643,19 @@ public class ChatVisual extends javax.swing.JFrame {
     private void btnSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSendActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnSendActionPerformed
+
+    private void btnTeacherActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTeacherActionPerformed
+        //Solicitar nueva etiqueta
+        preguntarQueAprender();
+        entrada.requestFocus();
+        ensenando = true;
+    }//GEN-LAST:event_btnTeacherActionPerformed
+
+
+    private void btnSongActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSongActionPerformed
+        cantar = !cantar;
+        cantar(cantar);
+    }//GEN-LAST:event_btnSongActionPerformed
 
     /**
      * @param args the command line arguments
@@ -532,6 +695,8 @@ public class ChatVisual extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSend;
+    private javax.swing.JButton btnSong;
+    private javax.swing.JButton btnTeacher;
     private javax.swing.JTextArea contenido;
     private javax.swing.JTextField entrada;
     private javax.swing.JLabel jLabel2;
